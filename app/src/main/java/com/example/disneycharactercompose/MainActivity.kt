c20bad72534fe8a23d1cf9f872305f55aa50aa51
sheldondcosta <ssheldondcosta@gmail.com>
2024-06-24 10:00:46 -0400

    Updated with view model implemented

--- app/src/main/java/com/example/disneycharactercompose/MainActivity.kt
+++ app/src/main/java/com/example/disneycharactercompose/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.disneycharactercompose.ui.screen.CharacterListScreen
 import com.example.disneycharactercompose.ui.theme.DisneyCharacterComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,12 +23,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DisneyCharacterComposeTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+               CharacterListScreen(viewModel())
             }
         }
     }
--- app/src/main/java/com/example/disneycharactercompose/ui/viewmodel/DisneyCharacterViewModel.kt
+++ app/src/main/java/com/example/disneycharactercompose/ui/viewmodel/DisneyCharacterViewModel.kt
@@ -1,14 +1,32 @@
 package com.example.disneycharactercompose.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.disneycharactercompose.data.model.DataModel
 import com.example.disneycharactercompose.data.repository.DisneyCharacterRepositoryImplementation
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DisneyCharacterViewModel @Inject constructor(
     private val repository: DisneyCharacterRepositoryImplementation
 ): ViewModel(){
+    private val _characters = MutableStateFlow<List<DataModel?>>(emptyList())
+    val characters: StateFlow<List<DataModel?>> = _characters
+
+    private val _selectCharacter = MutableStateFlow<DataModel?>(null)
+    val selectCharacter: StateFlow<DataModel?> = _selectCharacter
+
+    init {
+        viewModelScope.launch {
+            _characters.value = repository.getCharacterList()
+        }
+    }
+
+
 
 
 }
