9a6836f218ee3a64426c568c860ef106729c0047
sheldondcosta <ssheldondcosta@gmail.com>
2024-06-29 14:23:02 -0400

    Better UI still need to work on the ui a lot more

--- app/src/main/java/com/example/disneycharactercompose/ui/screen/CharacterListScreen.kt
+++ app/src/main/java/com/example/disneycharactercompose/ui/screen/CharacterListScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -56,7 +57,7 @@ fun CharacterListScreen(viewModel: DisneyCharacterViewModel = hiltViewModel()) {
 }
 @Composable
 fun CharacterItem(character: DataModel) {
-    Box(
+    Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
